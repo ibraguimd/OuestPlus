3f3757715e00d32d1853c146e78af6827b2fefbf
NEAU Alan <neau_a@sio.lan>
2021-06-08 14:48:36 +0200

    OuestPlus debut menu

--- page/template/menu.php
+++ page/template/menu.php
@@ -6,5 +6,27 @@
         </p>
     </a>
 </li>
-
-
+<li class="nav-item">
+    <a href="?route=taskAdd" class="nav-link">
+        <i class="nav-icon fas fa-plus-circle"></i>
+        <p class="text-light">
+            ajouter une tâche
+            </p>
+        </a>
+    </li>
+<?php
+$user = unserialize($_SESSION['user']);
+$html='';
+if($user->can('displayStat'))
+{
+    $html='<li class="nav-item">';
+    $html.='<a href="?route=stat" class="nav-link">';
+    $html.='<i class="nav-icon fas fa-tachometer-alt"></i>';
+    $html.='<p class="text-light">';
+    $html.='Graphique';
+    $html.='</p>';
+    $html.='</a>';
+    $html.='</li>';
+}
+echo $html;
+?>
